02620608f293d37795793ccb4013b9c5376a08f1
Dinara Faizullina <imfd@protonmail.com>
2018-04-07 23:05:40 +0300

    Updated Task1Tests from hw3

--- hw/hw3/Homework3Project/Homework3Library.Tests/Task1Tests.fs
+++ hw/hw3/Homework3Project/Homework3Library.Tests/Task1Tests.fs
@@ -6,50 +6,21 @@ open FsUnit
 
 module Task1Tests = 
 
-        [<Test>]
-        let ``countEvenNumbersByMap of [0; 1; 2; 3; 4; 5; 6] should return 4`` () = 
-            countEvenNumbersByMap [0; 1; 2; 3; 4; 5; 6] |> should equal 4
+        let testData = 
+            [
+                TestCaseData([] : (int list)).Returns(0)
+                TestCaseData([0; 1; 2; 3; 4; 5; 6]).Returns(4)
+                TestCaseData([-1; 0; -8; 4; -2; 7]).Returns(4)
+            ]
+        
+        [<TestCaseSource("testData")>]
+        let ``countEvenNumbersByMap of integer list returns the total of even numbers`` data = 
+            countEvenNumbersByMap data
 
-        [<Test>]
-        let ``countEvenNumbersByFilter of [0; 1; 2; 3; 4; 5; 6] should return 4`` () = 
-            countEvenNumbersByFilter [0; 1; 2; 3; 4; 5; 6] |> should equal 4
+        [<TestCaseSource("testData")>]
+        let ``countEvenNumbersByFilter of integer list returns the total of even numbers`` data = 
+            countEvenNumbersByFilter data
 
-        [<Test>]
-        let ``countEvenNumbersByFold of [0; 1; 2; 3; 4; 5; 6] should return 4`` () = 
-            countEvenNumbersByFold [0; 1; 2; 3; 4; 5; 6] |> should equal 4
-
-        [<Test>]
-        let ``countEvenNumbersByMap of [] should return 0`` () = 
-            countEvenNumbersByMap [] |> should equal 0
-
-        [<Test>]
-        let ``countEvenNumbersByFilter of [] should return 0`` () = 
-            countEvenNumbersByFilter [] |> should equal 0
-
-        [<Test>]
-        let ``countEvenNumbersByFold of [] should return 0`` () = 
-             countEvenNumbersByFold [] |> should equal 0
-
-        [<Test>]
-        let ``countEvenNumbersByMap of [0] should return 1`` () = 
-            countEvenNumbersByMap [0] |> should equal 1
-
-        [<Test>]
-        let ``countEvenNumbersByFilter of [0] should return 1`` () = 
-            countEvenNumbersByFilter [0] |> should equal 1
-
-        [<Test>]
-        let ``countEvenNumbersByFold of [0] should return 1`` () = 
-            countEvenNumbersByFold [0] |> should equal 1
-
-        [<Test>]
-        let ``countEvenNumbersByMap of [-1; 0; -8; 4; -2; 7] should return 4`` () = 
-            countEvenNumbersByMap [-1; 0; -8; 4; -2; 7] |> should equal 4
-
-        [<Test>]
-        let ``countEvenNumbersByFilter of [-1; 0; -8; 4; -2; 7] should return 4`` () = 
-            countEvenNumbersByFilter [-1; 0; -8; 4; -2; 7] |> should equal 4
-
-        [<Test>]
-        let ``countEvenNumbersByFold of [-1; 0; -8; 4; -2; 7] should return 4`` () = 
-            countEvenNumbersByFold [-1; 0; -8; 4; -2; 7] |> should equal 4
+        [<TestCaseSource("testData")>]
+        let ``countEvenNumbersByFold of integer list returns the total of even numbers`` data = 
+            countEvenNumbersByFold data
