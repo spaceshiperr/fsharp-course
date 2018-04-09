03c786aad9827a671753f713ce27760bd170bbdd
Dinara Faizullina <imfd@protonmail.com>
2018-04-09 23:01:48 +0300

    Added Task4.3 drafts

---
+++ hw/hw4/Task3/Task3Project/Task3ProjectLibrary/Task3.fs
@@ -0,0 +1,35 @@
+﻿namespace Homework4
+
+module Task3 = 
+    
+    /// <summary>
+    /// Defines the type for lambda term used in lambda expressions
+    /// </summary>
+    type Term<'a> = 
+        | Variable of 'a
+        | Application of Term<'a> * Term<'a>
+        | Abstraction of 'a * Term<'a>
+
+    /// <summary>
+    /// Replaces all occurrences of x with e1 in e2
+    /// </summary>
+    /// <param name="x">Variable of type Term that is being replaced</param>
+    /// <param name="exp1">Lambda expression of type Term with which the replacement of x is made</param>
+    /// <param name="exp2">Lambda expression of type Term in which the replacement is made</param>
+    let rec substitute x exp1 exp2 = 
+        match exp2 with
+        | Application (left, right) -> Application(substitute x exp1 left, substitute x exp1 right)
+        | Abstraction (v, f) when v <> x -> Abstraction(v, substitute x exp1 f)
+        | Variable (v) when x = v -> exp1
+        | _ as term -> term
+
+    /// <summary>
+    /// Beta-reduces the lambda expression
+    /// </summary>
+    /// <param name="exp">Expression to be beta-reduced</param>
+    let rec evaluate exp = 
+        match exp with 
+        | Application(Abstraction(v, f), right) -> substitute v right f
+        | Application(left, right) -> Application(evaluate left, evaluate right)
+        | Abstraction(v, f) -> Abstraction(v, evaluate f)
+        | _ as term -> term
---
+++ hw/hw4/Task3/Task3Project/Task3ProjectTests/Task3Tests.fs
@@ -0,0 +1,27 @@
+﻿namespace Homework4
+
+open Homework4.Task3
+open NUnit.Framework
+open FsUnit
+
+module Task3Tests = 
+    
+    let var x = Variable(x)
+    let x = var "x"
+    //let y = var "y"
+    //let z = var "z"
+    let I = Abstraction(x, Variable(x))
+    //let S = Abstraction(x, Abstraction(y, Abstraction(z, Application(Application(var x, var z), Application(var y, var z)))))
+    //let K = Abstraction(x, Abstraction(y, var x))
+    //let K' = Abstraction(x, Abstraction(y, var y))
+    
+    [<Test>]
+    let ``evaluate of lambda expression should return beta-reduced expression`` () = 
+        let exp = Application(Abstraction("f", Abstraction("x", Application(Application(Variable("f"), Variable("x")), Variable("x")))), Variable("+"))
+        let expected = Abstraction("x", Application (Application (Variable "+", Variable "x"), Variable "x"))
+        let actual = evaluate exp
+        actual |> should equal expected
+
+    [<Test>]
+    let ``evaluate of I I should return I`` () = 
+        evaluate <| Application(I, I) |> should equal I
