a123179ee66289da5aed4fca8b3fde668463e45e
Dinara Faizullina <imfd@protonmail.com>
2018-04-10 21:52:11 +0300

    Edited Task3Tests from hw4

--- hw/hw4/Task3/Task3Project/Task3ProjectTests/Task3Tests.fs
+++ hw/hw4/Task3/Task3Project/Task3ProjectTests/Task3Tests.fs
@@ -6,15 +6,6 @@ open FsUnit
 
 module Task3Tests = 
     
-    let var x = Variable(x)
-    let x = var "x"
-    //let y = var "y"
-    //let z = var "z"
-    let I = Abstraction(x, Variable(x))
-    //let S = Abstraction(x, Abstraction(y, Abstraction(z, Application(Application(var x, var z), Application(var y, var z)))))
-    //let K = Abstraction(x, Abstraction(y, var x))
-    //let K' = Abstraction(x, Abstraction(y, var y))
-    
     [<Test>]
     let ``evaluate of lambda expression should return beta-reduced expression`` () = 
         let exp = Application(Abstraction("f", Abstraction("x", Application(Application(Variable("f"), Variable("x")), Variable("x")))), Variable("+"))
@@ -24,4 +15,7 @@ module Task3Tests =
 
     [<Test>]
     let ``evaluate of I I should return I`` () = 
-        evaluate <| Application(I, I) |> should equal I
+        let I = Abstraction(Variable("x"), Variable(Variable("x")))
+        let expected = I
+        let actual = evaluate <| Application(I, I) 
+        actual |> should equal expected
