7155cb3e3f643a9059dd1f8a8a7162064e2be8cc
Dinara Faizullina <imfd@protonmail.com>
2018-05-31 12:01:42 +0300

    Added Task3 for test

---
+++ hw/TestApp/TestAppLib.Tests/Task3Tests.fs
@@ -0,0 +1,34 @@
+﻿namespace TestApp
+
+open TestApp.Task3
+open NUnit.Framework
+open FsUnit
+
+module Task3Tests = 
+    
+    [<Test>]
+    let ``IsEmpty of an initially empty int stack should return true`` () = 
+        let mutable stack = Stack<int>()
+        stack.IsEmpty() |> should equal true
+    
+    [<Test>]
+    let ``IsEmpty of cleared by user int stack should return false``() = 
+        let mutable stack = Stack<int>()
+        stack.Push 1
+        stack.Push 2
+        stack.Pop()
+        stack.Pop()
+        stack.IsEmpty() |> should equal true
+
+    [<Test>]
+    let ``IsEmpty of non empty string stack should return false``() = 
+        let mutable stack = Stack<string>()
+        stack.Push "hello"
+        stack.Push "world"
+        stack.IsEmpty() |> should equal false
+
+
+    [<Test>]
+    let ``Pop for an empty stack should raise System.InvalidOperationException``() = 
+        let mutable stack = Stack<string>()
+        (fun () -> stack.Pop() |> ignore) |> should throw typeof<System.InvalidOperationException>
---
+++ hw/TestApp/TestAppLib/Task3.fs
@@ -0,0 +1,20 @@
+﻿namespace TestApp
+
+    module Task3 = 
+
+        type Stack<'a> () =
+    
+            let mutable list  = []
+        
+            member this.IsEmpty() =
+                list.IsEmpty
+            
+            member this.Push x = 
+                list <- x :: list
+            
+            member this.Pop() =
+                match list with
+                | [] -> raise (System.InvalidOperationException "Trying to do the pop operation for an empty stack!")
+                | head:: tail -> list <- tail
+                                 head
+
