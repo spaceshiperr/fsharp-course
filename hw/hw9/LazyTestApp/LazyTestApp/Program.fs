d04fdf407537ad1ebd1b83d852e88166a2b5c802
Dinara Faizullina <imfd@protonmail.com>
2018-11-17 14:53:06 +0300

    Beginning hw9

---
+++ hw/hw9/LazyTestApp/LazyTestApp/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.Threading
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SingleThreadedLazy<'a when 'a: equality>(supplier: unit -> 'a) =
+    let mutable result: 'a = Unchecked.defaultof<'a>
+
+    interface ILazy<'a> with
+        member this.Get() = 
+            if (result <> Unchecked.defaultof<'a>)
+            then result <- supplier()
+            result                
+
+type LazyFactory() = 
+     static member CreateSingleThreadedLazy(supplier : unit -> 'a) =
+        SingleThreadedLazy<'a>(supplier) :> ILazy<'a>
+
+
+[<EntryPoint>]
+let main argv =
+    Console.ReadLine()
+    0
